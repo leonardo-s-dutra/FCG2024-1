befa805c1f093583963d3ba273d5cde08d4f746c
Leonardo Dutra <leo.dutra012@gmail.com>
2024-05-06 10:36:29 -0300

    Added ex8

---
+++ Lista1/ex8/main.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <Shader.h>
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+int setupGeometry();
+
+const GLuint WIDTH = 800;
+const GLuint HEIGHT = 600;
+
+
+int main()
+{
+	glfwInit();
+
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Exercicio 8", nullptr, nullptr);
+	glfwMakeContextCurrent(window);
+
+	glfwSetKeyCallback(window, key_callback);
+
+	if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cerr << "Failed to initialize GLAD" << "\n";
+	}
+
+	int width, height;
+	glfwGetFramebufferSize(window, &width, &height);
+	glViewport(0, 0, width, height);
+
+	Shader* shader = new Shader("VertexShader", "FragmentShader");
+
+	GLuint VAO = setupGeometry();
+	
+	GLint colorLoc = glGetUniformLocation(shader->ID(), "inputColor");
+	
+	shader->use();
+
+	while(!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+
+		glPointSize(10);
+
+		glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glBindVertexArray(VAO);
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		glDrawArrays(GL_POINTS, 0, 3);
+		glBindVertexArray(0);
+
+		glfwSwapBuffers(window);
+	}
+
+	glDeleteVertexArrays(1, &VAO);
+
+	glfwTerminate();
+	
+	delete shader;
+
+	return 0;
+}
+
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	}
+}
+
+
+int setupGeometry()
+{
+	GLfloat vertices[] = {
+	/*   x    y    z 	r    g    b	  */
+		 0.0, 0.5, 0.0, 1.0, 0.0, 0.0,	/* Vertice 1 */
+		-0.5, 0.0, 0.0, 0.0, 1.0, 0.0,	/* Vertice 2 */
+		 0.5, 0.0, 0.0, 0.0, 0.0, 1.0	/* Vertice 3 */
+	};
+
+	GLuint VBO, VAO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glGenVertexArrays(1, &VAO);
+
+	glBindVertexArray(VAO);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0); 
+
+	glBindVertexArray(0); 
+
+	return VAO;
+}
